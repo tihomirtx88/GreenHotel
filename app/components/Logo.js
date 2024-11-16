666394884ea6d8d70ec2c8606d330530add983c7
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-11-16 16:40:58 +0200

    Creating basic layout

---
+++ app/components/Logo.js
@@ -0,0 +1,12 @@
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-4 z-10">
+      <img src="/logo-green.jpeg" height="60" width="60" alt="Green Hotel logo" />
+      <span className="text-xl font-semibold text-primary-100">
+         Green Hotel
+      </span>
+    </a>
+  );
+}
+
+export default Logo;
