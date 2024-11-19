3a17f48d5e670970c8c23401a8fcd7b27ab47284
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-11-19 08:00:30 +0200

    bulding about, apartments and apartment card static pages

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -5,7 +5,7 @@ export default function Navigation(){
         <nav className="z-10 text-xl">
         <ul className="flex gap-16 items-center">
           <li>
-            <Link href="/cabins" className="hover:text-accent-400 transition-colors">
+            <Link href="/apartments" className="hover:text-accent-400 transition-colors">
               Rooms
             </Link>
           </li>
