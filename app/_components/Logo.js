ea75dd21cd74c02817b3d7dd569184fba5016ace
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-11-19 07:29:38 +0200

    Building home page

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/logo-green.jpeg";
+import logo from "@/public/green-logo-3-removebg-preview.png";
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10">
+    <Link href="/" className="flex items-center gap-4 z-10 w-[85px] h-[85px] rounded-full transition-transform duration-300 hover:scale-105">
       {/* With nesssery heigh and width */}
       {/* <Image src="/logo-green.jpeg" height="60" width="60" alt="Green Hotel logo" /> */}
       {/* Second way  */}
--- app/page.js
+++ app/page.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
-import Navigation from "./_components/Navigation";
-
+import Image from "next/image";
+import Homeimage from "@/public/green-3.jpg";
 export default function Home() {
   return (
-    <div>
-      <h1>Green Hotel. Welcome to paradise</h1>
+    <main className="mt-24">
+      <Image quality={80} className="w-full object-cover object-top" placeholder="blur" fill src={Homeimage} alt="Mountains and forests with two cabins" />
 
-      <Link href="/apartments">
-        Expolore best apartment for rest and holiday
-      </Link>
-    </div>
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 my-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury rooms
+        </Link>
+      </div>
+    </main>
   );
 }
