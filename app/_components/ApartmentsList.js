139e265db712bd1127363d9c734c3fec7be1eca7
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-11-22 09:42:45 +0200

    Streaming ui with suspens for apartment list

---
+++ app/_components/ApartmentsList.js
@@ -0,0 +1,18 @@
+import { getCabins } from "../_lib/data-service";
+import ApartmentCard from "./ApartmentCard";
+
+export default async function ApartmentsList(){
+
+    const apartments = await getCabins();
+
+    if(!apartments.length) return null;
+
+
+    return(
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+            {apartments.map((apartment) => (
+              <ApartmentCard apartment={apartment} key={apartment.id} />
+            ))}
+          </div>
+    );
+};
