34352f40a9d2a452bc79e1323eb9aba9bd9c8714
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-11-22 09:10:48 +0200

    Impelment loading with route segment

--- app/Loading.js
+++ app/Loading.js
@@ -1,7 +1,5 @@
+import Spinner from "./_components/Spinner";
+
 export default function Loading(){
-    return(
-        <div>
-            Loading data
-        </div>
-    );
+    return <Spinner/>;
 };
