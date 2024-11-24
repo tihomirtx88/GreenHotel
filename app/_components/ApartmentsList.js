14720db74ff54bd688e05e789e704471e324eeb7
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-11-24 16:45:44 +0200

    Using revalidate tool for revalidate chache data on apartments and about page

--- app/_components/ApartmentsList.js
+++ app/_components/ApartmentsList.js
@@ -1,18 +1,20 @@
+import { unstable_noStore } from "next/cache";
 import { getCabins } from "../_lib/data-service";
 import ApartmentCard from "./ApartmentCard";
 
-export default async function ApartmentsList(){
+export default async function ApartmentsList() {
+  // Refresh data cache
+  // unstable_noStore();
 
-    const apartments = await getCabins();
+  const apartments = await getCabins();
 
-    if(!apartments.length) return null;
+  if (!apartments.length) return null;
 
-
-    return(
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-            {apartments.map((apartment) => (
-              <ApartmentCard apartment={apartment} key={apartment.id} />
-            ))}
-          </div>
-    );
-};
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {apartments.map((apartment) => (
+        <ApartmentCard apartment={apartment} key={apartment.id} />
+      ))}
+    </div>
+  );
+}
