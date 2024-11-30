973f4e0d4eb78bf92d3dcb99fe922d394e7cf606
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-11-30 17:35:55 +0200

    building signin page and functilonality for singn in

--- app/_components/SigninButton.js
+++ app/_components/SigninButton.js
@@ -1,16 +1,23 @@
+import { signInAction } from "../_lib/actions";
+
+export const metadata = {
+   title: "Login"
+};
+
 function SignInButton() {
-    return (
-      <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
+  return (
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
         <img
-          src='https://authjs.dev/img/providers/google.svg'
-          alt='Google logo'
-          height='24'
-          width='24'
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
         />
         <span>Continue with Google</span>
       </button>
-    );
-  }
-  
-  export default SignInButton;
-  
+    </form>
+  );
+}
+
+export default SignInButton;
