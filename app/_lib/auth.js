2b0c531886e1be54d525aeacda023f53f61e67a5
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-11-30 17:19:24 +0200

    Crating protected route with middleware using next features

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -7,7 +7,13 @@ const authConfig = {
             clientId: process.env.AUTH_GOOGLE_ID,
             clientSecret: process.env.AUTH_GOOGLE_SECRET
         }),
-    ]
+    ],
+    callbacks: {
+        authorized({ auth, request }){
+            // If user exist return true athorwise just false
+            return !!auth?.user;
+        }
+    }
 };
 
 export const { auth, handlers: { GET, POST }} = NextAuth(authConfig);
---
+++ middleware.js
@@ -0,0 +1,14 @@
+// import { NextResponse } from "next/server";
+
+import { auth } from "@/app/_lib/auth";
+
+// export function middleware(request){
+//     return NextResponse.redirect(new URL("/about", request.URL));
+// };
+export const middleware = auth;
+
+export const config = {
+    matcher: [
+        "/account"
+    ]
+};
