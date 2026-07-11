38f87162ffbc9a99c4d4882a5e8a2d23508808e3
tihomirtx88 <tihomirtx88@gmail.com>
2026-07-11 18:25:42 +0300

    Responsive Logo components

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -4,14 +4,68 @@ import logo from "@/public/green-logo-3-removebg-preview.png";
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10 w-[85px] h-[85px] rounded-full transition-transform duration-300 hover:scale-105">
-      {/* With nesssery heigh and width */}
-      {/* <Image src="/logo-green.jpeg" height="60" width="60" alt="Green Hotel logo" /> */}
-      {/* Second way  */}
-      <Image src={logo} alt="Green Hotel logo" quality={100}/>
-      <span className="text-xl font-semibold text-primary-100">
-         Green Hotel
-      </span>
+      <Link
+      href="/"
+      className="
+        group
+        flex
+        items-center
+        gap-3
+        sm:gap-4
+        transition-transform
+        duration-300
+        hover:scale-105
+      "
+    >
+      <div
+        className="
+          relative
+          w-14
+          h-14
+          sm:w-16
+          sm:h-16
+          lg:w-20
+          lg:h-20
+          flex-shrink-0
+        "
+      >
+        <Image
+          src={logo}
+          alt="Green Hotel logo"
+          fill
+          priority
+          quality={100}
+          sizes="(max-width:768px) 56px, (max-width:1024px) 64px, 80px"
+          className="object-contain"
+        />
+      </div>
+
+      <div className="flex flex-col leading-none">
+        <span
+          className="
+            text-lg
+            sm:text-xl
+            lg:text-2xl
+            font-bold
+            tracking-wide
+            text-primary-100
+          "
+        >
+          Green Hotel
+        </span>
+
+        <span
+          className="
+            hidden
+            sm:block
+            text-xs
+            lg:text-sm
+            text-primary-400
+          "
+        >
+          Luxury Apartments
+        </span>
+      </div>
     </Link>
   );
 }
