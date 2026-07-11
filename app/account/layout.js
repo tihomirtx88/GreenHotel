777014d9d3c4a7cd8e571b77313b4d9c3cb1e7aa
tihomirtx88 <tihomirtx88@gmail.com>
2026-07-11 19:14:44 +0300

    Responsive Layout page

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,10 +1,24 @@
 import SideNavigation from "@/app/_components/SideNavigations";
 
-export default function Layout({children}){
-    return(
-        <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
-          <SideNavigation/>
-          <div className="py-1">{children}</div>
-        </div>
-    );
-};
+export default function Layout({ children }) {
+  return (
+    <div
+      className="
+        grid
+        grid-cols-1
+        lg:grid-cols-[16rem_1fr]
+
+        gap-6
+        lg:gap-12
+
+        min-h-full
+      "
+    >
+      <aside>
+        <SideNavigation />
+      </aside>
+
+      <main className="py-2 min-w-0">{children}</main>
+    </div>
+  );
+}
