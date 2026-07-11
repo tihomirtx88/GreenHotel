b8ed03796967b1b9a6c1db333b92e1225f1917f8
tihomirtx88 <tihomirtx88@gmail.com>
2026-07-11 18:21:57 +0300

    Responsive Header

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -3,12 +3,36 @@ import Navigation from "./Navigation";
 
 function Header() {
     return (
-      <header className='border-b border-primary-900 px-8 py-5'>
-        <div className='flex justify-between items-center max-w-7xl mx-auto'>
-          <Logo />
-          <Navigation />
-        </div>
-      </header>
+       <header
+      className="
+        sticky
+        top-0
+        z-50
+        border-b
+        border-primary-800
+        bg-primary-950/95
+        backdrop-blur-md
+        shadow-md
+      "
+    >
+      <div
+        className="
+          max-w-7xl
+          mx-auto
+          px-4
+          sm:px-6
+          lg:px-8
+          h-20
+          flex
+          items-center
+          justify-between
+        "
+      >
+        <Logo />
+
+        <Navigation />
+      </div>
+    </header>
     );
   }
   
