a10e84da61f6199a59ded28fa5f57f0e488ac4ac
tihomirtx88 <tihomirtx88@gmail.com>
2026-07-12 10:15:43 +0300

    Cheks

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -6,3 +6,5 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 console.log("Supabase URL:", supabaseUrl)
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_KEY?.slice(0, 20));
